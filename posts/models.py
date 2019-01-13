4cad85a78812303330f5a15b6cde8b61760148b0
chrisaor <pchpch0070@gmail.com>
2019-01-13 16:43:59 +0900

    ADD post model field 'body'

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
